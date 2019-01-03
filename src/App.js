d3ff37d1320f9f381a8b8880cbe75c77066d3626
Nikhil Swaminathan <nsswamin@amazon.com>
2019-01-02 17:30:12 -0800

    Got broadcaster working

--- src/App.js
+++ src/App.js
@@ -1,25 +1,87 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import Amplify, {API,graphqlOperation} from 'aws-amplify';
+import aws_exports from './aws-exports'; // specify the location of aws-exports.js file on your project
+Amplify.configure(aws_exports);
+
+const createMessage = `mutation createMessage($message: String!){
+  createMessage(input:{message:$message}) {
+    id
+    message
+  }
+}
+`;
+
+const onCreateMessage = `subscription onCreateMessage {
+  onCreateMessage {
+    __typename
+    message
+  }
+}`;
 
 class App extends Component {
+
+  constructor(props){
+     super(props);
+     this.state={
+        message:"",
+        value:"",
+        display:false
+     };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  async componentDidMount(){
+    this.subscription = API.graphql(graphqlOperation(onCreateMessage)).subscribe({
+      next: (event) => {
+          console.log("Subscription: "+event.value.data);
+          this.setState({display: true});
+          this.setState({message: event.value.data.onCreateMessage.message});
+        }
+    });
+  }
+
+  handleChange(event) {
+    this.setState({value:event.target.value});
+  }
+
+  async handleSubmit(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    const message = {
+      "message":this.state.value
+    }
+    await API.graphql(graphqlOperation(createMessage, message));
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <img src={logo} className="App-logo" alt="logo" />
+        <div className="jumbotron jumbotron-fluid p-0">
+          <h3 className="display-4">Broadcaster</h3>
+        </div>
+        <br/>
+        <div className="container">
+          <form onSubmit={this.handleSubmit}>
+          <div className="form-group">
+            <input className="form-control form-control-lg"type="text" value={this.state.value} onChange={this.handleChange} />
+          </div>
+          <input type="submit" value="Submit" className="btn btn-primary"/>
+          </form>
+        </div>
+        <br/>
+        {this.state.display ?  
+          <div className="container">
+            <div className="card bg-success">
+              <h3 className="card-text text-white p-2">{this.state.message}</h3>
+            </div>
+          </div>
+        : null }
       </div>
     );
   }
